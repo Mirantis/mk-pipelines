5c4a471129248874cd7ae6ae9df9eb9393ea4603
Filip Pytloun <filip@pytloun.cz>
2017-02-27 22:41:32 +0100

    Wait longer to start services
    Change-Id: Ice8cf0b6514c490e4b7aeb93fa0614b9c02b6b01

--- cicd-lab-pipeline.groovy
+++ cicd-lab-pipeline.groovy
@@ -123,8 +123,8 @@ node {
         salt.enforceState(saltMaster, 'I@docker:swarm:role:master', 'docker.client')
 
         // XXX: Hack to fix dependency of gerrit on mysql
-        print salt.cmdRun(saltMaster, 'I@docker:swarm:role:master', "docker service rm gerrit; rm -rf /srv/volumes/gerrit/*")
-        sleep(10)
+        print salt.cmdRun(saltMaster, 'I@docker:swarm:role:master', "docker service rm gerrit; sleep 5; rm -rf /srv/volumes/gerrit/*")
+        sleep(30)
         print salt.cmdRun(saltMaster, 'I@docker:swarm:role:master', "apt-get install -y mysql-client; mysql -ppassword -h172.16.10.254 -e'drop database gerrit;create database gerrit;'")
         salt.enforceState(saltMaster, 'I@docker:swarm:role:master', 'docker.client')
         // ---- cut here (end of hack) ----
@@ -141,7 +141,7 @@ node {
                     }
                 }
             }
-            sleep(10)
+            sleep(30)
         }
     }
 
