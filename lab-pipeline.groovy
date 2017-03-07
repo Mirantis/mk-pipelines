aa8f553e8585f859509e9a14ac6dfbfc0c1695a1
Tomáš Kukrál <tkukral@mirantis.com>
2017-03-07 13:34:39 +0100

    add mysql.client step
    Change-Id: I7bd2d23623e0d77b7de944b3e84f241b8d0cba35

--- lab-pipeline.groovy
+++ lab-pipeline.groovy
@@ -282,6 +282,9 @@ timestamps {
                     salt.runSaltProcessStep(saltMaster, 'I@galera:master', 'mysql.status')
                     salt.runSaltProcessStep(saltMaster, 'I@galera:slave', 'mysql.status')
 
+                    // Setup mysql client
+                    salt.enforceState(saltMaster, 'I@galera:master', 'mysql.client', true)
+
                     // Install haproxy
                     salt.enforceState(saltMaster, 'I@haproxy:proxy', 'haproxy', true)
                     salt.runSaltProcessStep(saltMaster, 'I@haproxy:proxy', 'service.status', ['haproxy'])
