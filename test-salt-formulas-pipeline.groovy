843e7d9b7706f982e25e0ff5ce53fe102832c304
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-05-11 11:05:24 +0200

    temporarily comment stop old tests phase
    Change-Id: I8d53a553ed6baf19faf783526dd4305d0184b2a4

--- test-salt-formulas-pipeline.groovy
+++ test-salt-formulas-pipeline.groovy
@@ -29,15 +29,15 @@ def checkouted = false;
 
 node("python&&docker") {
   try{
-    stage("stop old tests"){
-      if (gerritRef) {
-        def runningTestBuildNums = _getRunningTriggeredTestsBuildNumbers(env["JOB_NAME"], GERRIT_CHANGE_NUMBER, GERRIT_PATCHSET_NUMBER)
-        for(int i=0; i<runningTestBuildNums.size(); i++){
-          common.infoMsg("Old test with run number ${runningTestBuildNums[i]} found, stopping")
-          Jenkins.instance.getItemByFullName(env["JOB_NAME"]).getBuildByNumber(runningTestBuildNums[i]).finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"));
-        }
-      }
-    }
+    //stage("stop old tests"){
+    //  if (gerritRef) {
+    //    def runningTestBuildNums = _getRunningTriggeredTestsBuildNumbers(env["JOB_NAME"], GERRIT_CHANGE_NUMBER, GERRIT_PATCHSET_NUMBER)
+    //    for(int i=0; i<runningTestBuildNums.size(); i++){
+    //      common.infoMsg("Old test with run number ${runningTestBuildNums[i]} found, stopping")
+    //      Jenkins.instance.getItemByFullName(env["JOB_NAME"]).getBuildByNumber(runningTestBuildNums[i]).finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"));
+    //    }
+    //  }
+    //}
     stage("checkout") {
       if (gerritRef) {
         // job is triggered by Gerrit
