23ba7d7ab6677e8b7968efa3a9a2a644efcedf23
kairat_kushaev <kkushaev@mirantis.com>
2017-05-18 16:13:56 +0400

    Add Keystone v3 envs to k8s-simple-deploy pipeline
    Add possibility to specify project_domain and user domain for
    k9s-simple-deploy pipeline. when patch will have merged the pipeline
    van work with keystone v3 OS environment.
    Depends-On: I2f18c0ad29678d96eae4b8986585f0c71e4a0d30
    
    Change-Id: I55c0eb7e54a9ff6511cc91b65d4bfbb8dcc9dad6

--- mk-k8s-simple-deploy-pipeline.groovy
+++ mk-k8s-simple-deploy-pipeline.groovy
@@ -54,7 +54,8 @@ node {
     }
 
     stage('Connect to OpenStack cloud') {
-        openstackCloud = openstack.createOpenstackEnv(OPENSTACK_API_URL, OPENSTACK_API_CREDENTIALS, OPENSTACK_API_PROJECT)
+        openstackCloud = openstack.createOpenstackEnv(OPENSTACK_API_URL, OPENSTACK_API_CREDENTIALS, OPENSTACK_API_PROJECT,
+        OPENSTACK_API_PROJECT_DOMAIN_ID, OPENSTACK_API_USER_DOMAIN_ID)
         openstack.getKeystoneToken(openstackCloud, openstackEnv)
     }
 
