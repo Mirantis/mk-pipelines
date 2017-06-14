aebfa9c7ebb85baf4bb06badd50466bfc6386bb3
Filip Pytloun <filip@pytloun.cz>
2017-06-14 16:12:57 +0200

    Fix undefined variable
    Change-Id: Icfc1ddfafc467be190e55b4cfcce0fd82eedf04c

--- cicd-lab-pipeline.groovy
+++ cicd-lab-pipeline.groovy
@@ -167,7 +167,7 @@ timestamps {
                 // services are deployed
                 // XXX: for some weird unknown reason, refresh_pillar is
                 // required to execute here
-                salt.runSaltProcessStep(master, 'I@aptly:publisher', 'saltutil.refresh_pillar', [], null, true)
+                salt.runSaltProcessStep(saltMaster, 'I@aptly:publisher', 'saltutil.refresh_pillar', [], null, true)
                 salt.enforceState(saltMaster, 'I@aptly:publisher', 'aptly.publisher', true)
                 retry(3) {
                     sleep(5)
