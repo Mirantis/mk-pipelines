0bae46708aaf0d8e25e3d9dcabb70272306cfa7a
chnyda <chnyda@mirantis.com>
2017-06-20 13:39:35 +0200

    Fix build of libvirt-exporter
    Change-Id: I7a5cdd5b069d96f4f79e5ca2173751f6440f278d

--- build-debian-packages-libvirt-exporter.groovy
+++ build-debian-packages-libvirt-exporter.groovy
@@ -1,6 +1,7 @@
 def common = new com.mirantis.mk.Common()
 def git = new com.mirantis.mk.Git()
 def aptly = new com.mirantis.mk.Aptly()
+def dockerLib = new com.mirantis.mk.Docker()
 
 def timestamp = common.getDatetime()
 def version = "0.1~${timestamp}"
@@ -26,16 +27,24 @@ node('docker') {
 
         stage("build binary") {
             dir("libvirt-exporter-${version}") {
-                sh("""sed -i 's/VERSION/${version}/g' debian/changelog &&
-		    scripts/build.py --package --version=\"${version}\" --platform=linux --arch=amd64""")
+                sh("sed -i 's/VERSION/${version}/g' debian/changelog && ./build_static.sh")
+            }
+        }
+
+        def img = dockerLib.getImage("tcpcloud/debian-build-ubuntu-xenial")
+        stage("build package") {
+            img.inside("-u root:root") {
+                sh("apt-get update && apt-get install ruby ruby-dev && gem install fpm")
+                sh("cd libvirt-exporter-${version} && scripts/build.py --package --version=\"${version}\" --platform=linux --arch=amd64")
             }
             archiveArtifacts artifacts: "libvirt-exporter-${version}/build/*.deb"
         }
+
         if (UPLOAD_APTLY.toBoolean()) {
             lock("aptly-api") {
                 stage("upload") {
                     def buildSteps = [:]
-                    def debFiles = sh script: "ls *.deb", returnStdout: true
+                    def debFiles = sh(script: "ls libvirt-exporter-${version}/build/*.deb", returnStdout: true)
                     def debFilesArray = debFiles.trim().tokenize()
                     def workspace = common.getWorkspace()
                     for (int i = 0; i < debFilesArray.size(); i++) {
@@ -54,6 +63,10 @@ node('docker') {
                     aptly.publish(APTLY_URL)
                 }
             }
+        }
+
+        img.inside("-u root:root") {
+            sh("rm -rf * || true")
         }
 
     } catch (Throwable e) {
