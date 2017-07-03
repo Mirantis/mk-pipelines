5bd1f2cbd05960d2f850ccc90e9e856b4fb29e1c
Filip Pytloun <filip@pytloun.cz>
2017-07-03 11:11:16 +0200

    Fix k8s-deployments pipeline
    Change-Id: I587fe025b03306f18d3437840b0e841340f7f0e0

--- deploy-k8s-deployments.groovy
+++ deploy-k8s-deployments.groovy
@@ -4,13 +4,16 @@ def salt = new com.mirantis.mk.Salt()
 targetExpression = TARGET_MINIONS ? TARGET_MINIONS : "E@kvm01.*"
 
 node() {
-    def master = salt.connection(SALT_URL)
+    def saltMaster
+    stage('Connect to Salt master') {
+        saltMaster = salt.connection(SALT_MASTER_URL, SALT_MASTER_CREDENTIALS)
+    }
     common.infoMsg("Enforcing kubernetes state..")
     stage("Update k8s control") {
         salt.enforceState(
-            master,
-            ['expression': targetExpression, 'type': 'compound'],
-            ['kubernetes.control'],
+            saltMaster,
+            targetExpression,
+            'kubernetes.control',
             true
         )
     }
@@ -18,7 +21,7 @@ node() {
         common.info("Setting up components..")
         out = salt.cmdRun(
             master,
-            ['expression': targetExpression, 'type': 'compound'],
+            targetExpression,
             '/bin/bash -c \'find /srv/kubernetes/ -type d | grep -v jobs | while read i; do ls $i/*.yml &>/dev/null && (set -x; hyperkube kubectl apply -f $i || echo Command failed; set +x); done; jobs=$(hyperkube kubectl get jobs -o name); find /srv/kubernetes/jobs -type f -name "*.yml" | while read i; do name=$(grep "name:" $i | head -1 | awk "{print $NF}"); echo $jobs|grep $name >/dev/null || (set -x; hyperkube kubectl apply -f $i || echo Command failed; set +x);done\''
         )
         for (entry in out['return']) {
