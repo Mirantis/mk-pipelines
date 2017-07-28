1cd5d7c7337191edf7b5dfccad7eed86a465b309
Vasyl Saienko <vsaienko@mirantis.com>
2017-07-28 18:39:01 +0300

    Add openstack component testing job
    Change-Id: I2745868a94db4999435574519ee88503a8484310

---
+++ test-openstack-component-pipeline.groovy
@@ -0,0 +1,31 @@
+/**
+
+ * Launch system tests against new package.
+
+ * Flow parameters:
+ *   CREDENTIALS_ID
+ *   EXTRA_FORMULAS
+ *   FORMULAS_REVISION
+ *   FORMULAS_SOURCE
+ *   SALT_OPTS
+ *   STACK_DEPLOY_JOB
+
+**/
+def common = new com.mirantis.mk.Common()
+def gerrit = new com.mirantis.mk.Gerrit()
+
+
+timestamps {
+    node {
+        def cred = common.getCredentials(CREDENTIALS_ID, 'key')
+        def gerritChange = gerrit.getGerritChange(cred.username, GERRIT_HOST, GERRIT_CHANGE_NUMBER, CREDENTIALS_ID, true)
+
+        stage('Trigger deploy job') {
+            build(job: STACK_DEPLOY_JOB, parameters: [
+                [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: 'mcp-oscore'],
+                [$class: 'StringParameterValue', name: 'STACK_TEST', value: ''],
+                [$class: 'BooleanParameterValue', name: 'TEST_DOCKER_INSTALL', value: false]
+            ])
+        }
+    }
+}
