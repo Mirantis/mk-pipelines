e78592957de191687c0310464b65159058b3b26a
Tomáš Kukrál <tkukral@mirantis.com>
2017-08-11 13:52:06 +0200

    fix tempest test runs
    Change-Id: Ibbebf615a818ba45fad95021191f63574c453b3d

--- test-service.groovy
+++ test-service.groovy
@@ -77,20 +77,16 @@ node("python") {
         }
 
         if (common.checkContains('TEST_SERVICE', 'openstack')) {
-            // if (common.checkContains('TEST_DOCKER_INSTALL', 'true')) {
-            //     test.install_docker(saltMaster, TEST_TEMPEST_TARGET)
-            // }
-            // stage('Run OpenStack tests') {
-            //     test.runTempestTests(saltMaster, TEST_TEMPEST_IMAGE, TEST_TEMPEST_TARGET, TEST_TEMPEST_PATTERN)
-            // }
+            if (common.checkContains('TEST_DOCKER_INSTALL', 'true')) {
+                test.install_docker(saltMaster, TEST_TEMPEST_TARGET)
+            }
 
-            //stage('Copy Tempest results to config node') {
-            //    test.copyTempestResults(saltMaster, TEST_TEMPEST_TARGET)
-            //
+            stage('Run OpenStack tests') {
+                test.runTempestTests(saltMaster, TEST_TEMPEST_IMAGE, TEST_TEMPEST_TARGET, TEST_TEMPEST_PATTERN)
+            }
 
             writeFile(file: 'report.xml', text: salt.getFileContent(saltMaster, TEST_TEMPEST_TARGET, '/root/report.xml')
             junit(keepLongStdio: true, testResults: 'report.xml', healthScaleFactor, TEST_JUNIT_RATIO)
-
         }
 
     } catch (Throwable e) {
