6ff5a136c977457b53cf14e35f93fb276ca8cea2
Jakub Josef <jakub.josef@gmail.com>
2017-08-16 18:53:46 +0200

    Removed extra print in cicd lab template
    Change-Id: Ic3c6c924e739d6badfa360b31d011dc011739fb4

--- cicd-lab-pipeline.groovy
+++ cicd-lab-pipeline.groovy
@@ -305,7 +305,6 @@ node {
             salt.enforceState(saltMaster, 'I@nginx:server', 'nginx')
 
             def failedSvc = salt.cmdRun(saltMaster, '*', """systemctl --failed | grep -E 'loaded[ \t]+failed' && echo 'Command execution failed' || true""")
-            print common.prettyPrint(failedSvc)
             if (failedSvc =~ /Command execution failed/) {
                 common.errorMsg("Some services are not running. Environment may not be fully functional!")
             }
