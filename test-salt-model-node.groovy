105689e6539ba7e70a8e06b1d15ed12e1ae807fc
chnyda <chnyda@mirantis.com>
2017-11-30 12:38:20 +0100

    Add missing dependency
    Change-Id: I08d24423999884ac1ebf5bb821021e70b1331762

--- test-salt-model-node.groovy
+++ test-salt-model-node.groovy
@@ -62,7 +62,7 @@ throttle(['test-model']) {
           def workspace = common.getWorkspace()
           common.infoMsg("Running salt model test for node ${NODE_TARGET} in cluster ${CLUSTER_NAME}")
           try {
-            setupAndTestNode(NODE_TARGET, CLUSTER_NAME, EXTRA_FORMULAS, workspace, FORMULAS_SOURCE, FORMULAS_REVISION, MAX_CPU_PER_JOB.toInteger(), RECLASS_IGNORE_CLASS_NOTFOUND, LEGACY_TEST_MODE, APT_REPOSITORY, APT_REPOSITORY_GPG)
+            saltModelTesting.setupAndTestNode(NODE_TARGET, CLUSTER_NAME, EXTRA_FORMULAS, workspace, FORMULAS_SOURCE, FORMULAS_REVISION, MAX_CPU_PER_JOB.toInteger(), RECLASS_IGNORE_CLASS_NOTFOUND, LEGACY_TEST_MODE, APT_REPOSITORY, APT_REPOSITORY_GPG)
           } catch (Exception e) {
             if (e.getMessage() == "script returned exit code 124") {
               common.errorMsg("Impossible to test node due to timeout of salt-master, ABORTING BUILD")
