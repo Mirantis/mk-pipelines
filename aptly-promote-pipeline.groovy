6f27d1b48489950e22e9e73a66110def3c511b3c
Kirill Mashchenko <kmashchenko@mirantis.com>
2018-12-12 15:38:00 +0400

    disable restrictions to run aptly-promote-pipeline
    Change-Id: I645443838e02171c098ab147de7d0c6fff48bf26

--- aptly-promote-pipeline.groovy
+++ aptly-promote-pipeline.groovy
@@ -28,14 +28,6 @@ def insufficientPermissions = false
 timeout(time: 12, unit: 'HOURS') {
     node("docker&&hardware") {
         try {
-
-            if (("testing" in TARGET || "proposed" in TARGET) && !jenkinsUtils.currentUserInGroup(["release-engineering", "aptly-promote-users"])) {
-                insufficientPermissions = true
-                throw new Exception("Only release-engineering or aptly-promote-users can perform promote to testing.")
-            } else if (!jenkinsUtils.currentUserInGroup(["release-engineering"])) {
-                insufficientPermissions = true
-                throw new Exception("Only release-engineering team can perform promote.")
-            }
             stage("promote") {
                 // promote is restricted to users in aptly-promote-users LDAP group
                 lock("aptly-api") {
