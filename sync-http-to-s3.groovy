038b81fbc4a95e14a8f1ad6834799ac21a6070f7
Ivan Udovichenko <iudovichenko@mirantis.com>
2020-10-05 23:57:27 +0300

    [sync-http-to-s3] Add more tries to wget
    Change-Id: I83b14ef54a377693a9771073694f73bed05f2777
    Related-PROD: https://mirantis.jira.com/browse/RE-391

--- sync-http-to-s3.groovy
+++ sync-http-to-s3.groovy
@@ -19,7 +19,7 @@ node("docker") {
                                   usernameVariable: 'S3_ACCESS_KEY', passwordVariable: 'S3_SECRET_KEY']]) {
                     img.inside("-e S3_ACCESS_KEY=${S3_ACCESS_KEY} -e S3_SECRET_KEY=${S3_SECRET_KEY}") {
                         common.retry(3, 5) {
-                            sh(script: "wget --progress=dot:giga -O ${filename} ${url}", returnStdout: true)
+                            sh(script: "wget --progress=dot:giga --tries=50 -O ${filename} ${url}", returnStdout: true)
                             sh(script: "/usr/local/bin/s4cmd put ${opts} ${filename} ${DEST}/${filename}", returnStdout: true)
                         }
                     }
