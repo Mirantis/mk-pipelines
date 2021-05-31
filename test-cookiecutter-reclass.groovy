30411c223a2de1e0abf78f2c7f39c12781ab1f67
Vladimir Khlyunev <vkhlyunev@mirantis.com>
2021-05-31 16:38:57 +0000

    Revert "Pin some reclass model tests to old jenkins nodes"
    This reverts commit 56326acab2d7c82ed12a3b6e5292e2a0bd07419e.
    
    Reason for revert: not required; PROD-36373
    
    Change-Id: I6a4f7a4efe73979d6d5a73fca604746d4163cef3

--- test-cookiecutter-reclass.groovy
+++ test-cookiecutter-reclass.groovy
@@ -27,7 +27,7 @@ if (extraVarsYAML) {
     extraVars = [:]
 }
 
-slaveNode = env.SLAVE_NODE ?: 'old16.04'
+slaveNode = env.SLAVE_NODE ?: 'virtual'
 checkIncludeOrder = env.CHECK_INCLUDE_ORDER ?: false
 
 // Global var's
